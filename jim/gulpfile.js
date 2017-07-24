b7f40d48da41df86a01f49ea90ef3889ba0a62da
Daniel Chao <daniel.h.chao@gmail.com>
2017-07-23 19:40:50 -0700

    jim

---
+++ jim/gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp');
+const watch = require('gulp-watch');
+const { execSync } = require('child_process');
+
+gulp.task('watch', () => {
+  watch('./**/*.jack')
+    .on('data', () => {
+      console.log("EXEC SYNC");
+      execSync("../../tools/JackCompiler.sh source/", {
+        stdio: "inherit"
+      });
+    });
+});
